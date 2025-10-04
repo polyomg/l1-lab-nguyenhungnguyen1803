4ca6b852977a1b32760cbb9522359f78f041d633
Nguyen <hungnguyenlq123@gmail.com>
2025-10-04 09:05:56 +0700

    Lab2_NguyenHungNguyen_TS00640

--- Lab2/src/test/java/poly/edu/lab2/Lab2ApplicationTests.java
+++ Lab2/src/test/java/poly/edu/lab2/Lab2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.poly.lab2;
+package poly.edu.lab2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Lab2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
